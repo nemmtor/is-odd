4fa08b67bc4d47ba8ad4f216a63d1dde198bbdc9
nemmtor <kacper0witas@gmail.com>
2024-11-20 22:37:31 +0100

    chore: more jsdocs

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,14 @@
 /** Checks if number is odd
  * @param value number to be checked
  * @returns information if number is odd
+ * @example
+ * ```ts
+ * const result = isOdd(3); // true
+ * ````
+ * @example
+ * ```ts
+ * const result = isOdd(4); // false
+ * ````
  */
 export const isOdd = (value: number): boolean => {
   const remainder = Math.abs(value % 2);
